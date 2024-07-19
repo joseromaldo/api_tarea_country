eb8324f013bc009c63f52f80e9e70d05ce1dd583
Maldonado <joseromaldo.1997@gmail.com>
2024-07-19 09:08:28 -0600

    Envío

---
+++ script.js
@@ -0,0 +1,49 @@
+// IMPORTACION DE LIBRERIAS 
+// DECLARACION DE VARIABLES Y CONSTANTES
+const formulario = document.querySelector('form');
+const send = document.getElementById('send');
+const info = document.getElementById('info');
+
+info.style.display = 'none'
+
+// DECLARACION DE FUNCIONES
+const consultarPais = async (e) => {
+     e.preventDefault();
+
+    let name = document.getElementById('name').value.trim()
+    
+    if(name == ''){
+        alert('Ingrese un nombre')
+        return;
+    }
+    send.disabled = true;
+    try {
+        const consulta = await fetch(`https://restcountries.com/v3.1/name/${name}`, {
+            method: 'GET'
+        })
+
+        if (consulta.status == 200){
+            const datos = await consulta.json();
+            console.log(datos);
+            document.getElementById('nombrePais').textContent = `Country name: ${datos[0].name.common}`;
+            document.getElementById('capitalPais').textContent = `Capital City: ${datos[0].capital}`;
+            document.getElementById('continentePais').textContent = `Continent: ${datos[0].region}`;
+            document.getElementById('imgPais').src = datos[0].coatOfArms.png;
+
+            info.style.display = '';
+
+        } else {
+            alert('Ingrese correctamente el nombre del país')
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+// MANEJO DE EVENTOS
+formulario.addEventListener('submit', consultarPais)
